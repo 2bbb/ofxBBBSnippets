d06b2bc75b1cc7eb8f16ff740a1127eecd3ec278
2bit <i@2bit.jp>
2025-02-12 11:01:43 +0900

    fix

--- src/ofxLimitedLife.h
+++ src/ofxLimitedLife.h
@@ -8,9 +8,9 @@
 #include <bbb/snippets/limited_life.hpp>
 
 template <typename base_type, float time_func() = ofGetElapsedTimef>
-using ofxLimitedLifeInjector = bbb::LimitedLifeInjector<base_type, time_func>;
+using ofxLimitedLifeInjector = bbb::limited_life_injector<base_type, time_func>;
 
 template <typename base_type, float time_func() = ofGetElapsedTimef>
-using ofxLimitedLife = bbb::LimitedLife<base_type, time_func>;
+using ofxLimitedLife = bbb::limited_life<base_type, time_func>;
 
 #endif // OFXLIMITEDLIFE_H
